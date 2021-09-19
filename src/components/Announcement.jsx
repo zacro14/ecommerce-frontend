c3d055823beb7dcd206edc4df37961c7df3b1a46
Junel <sacrojunel@gmail.com>
2021-09-19 12:23:44 +0800

    change bg-color/add close icon

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,20 +1,28 @@
 import styled from "styled-components"
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 const Container = styled.div`
     height: 40px;
-    background-color: #EEC015;
+    background-color:  #EEC015;
     color: #000000;
     display:  flex;
     align-items: center;
     justify-content:center;
-    cursor: pointer;
     font-size:14px;
     font-weight:500;
+`
+const Close =styled.span`
+    justify-content: flex-end;
+    cursor: pointer;
+    color: #303F42;
 `
 
 const Announcement = () => {
     return (
         <Container>
                 Super Deal! Free Shipping on Orders with Minimum Spend of  &#8369;199
+                <Close>
+                    <CloseOutlinedIcon />
+                </Close>
         </Container>
     )
 }
