9bbdd901705a01df0e72ef3b0fdb5ea303067d38
Junel <sacrojunel@gmail.com>
2021-09-26 16:23:50 +0800

    add input styles

--- src/components/newsletter/newsletter.styled.js
+++ src/components/newsletter/newsletter.styled.js
@@ -6,17 +6,49 @@ export const Container = styled.div`
     align-items: center;
     justify-content:center;
     flex-direction: column;
-    background-color: #EEC015;
+    background-color: #F0F2F5;
     color: #000000;
 `;
 export const Title = styled.h1`
-    font-size:70px;
+    font-size:50px;
 `;
 export const  Description= styled.div`
-    font-size:24px;
+    font-size:15px;
     font-weight:300;
     margin-bottom: 20px;
 `;
-export const Input = styled.input``;
-export const Button = styled.button``;
-export const InputContainer = styled.div``;
+
+export const Button = styled.button`
+    flex:1;
+    border: none;
+    background-color: #000000;
+    color:  #FFFFFF;
+    cursor: pointer;
+
+    &:hover{
+        background-color:#000000;   
+        color: #EEC015;     
+    }
+
+`;
+export const InputContainer = styled.div`
+    width:50%;
+    height:45px;
+    background-color: white;   
+    display:flex;
+    justify-content:space-between;
+    border: 1px solid #000000;
+   
+`;
+export const Input = styled.input`
+    border: none;
+    flex:8;
+    padding-left:10px;
+
+    &:focus{
+        outline:none;
+        ${InputContainer}{
+            border: 1px solid #EEC015;
+        }
+    }
+`;
