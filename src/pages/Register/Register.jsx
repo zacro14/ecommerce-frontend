8403712fcc3ebe6d835bcee56c7babebb260c960
Junel <sacrojunel@gmail.com>
2021-09-30 20:10:08 +0800

    add register

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,135 @@
+
+import { Apple, Facebook, Google, VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material"
+import { useState } from "react"
+import Footer from "../../components/footer/Footer"
+import HeaderLogin from "../../components/headerlogin/HeaderLogin"
+import { Container, Desc, DivLeft,
+    FormContainer, Icon, IconLabel, Input, InputContainer, 
+    LabelNew, LabelSignup, LeftContainer, LoginButton, 
+    LoginDescContainer, Or, RightContainer, Separator, SeparatorContainer, 
+    SignupContainer, SocialButton, SocialContainer, TermsPrivacy, TermsPrivacyContainer, Title, 
+    TitleCreateAccount,TitleLogin, Visibility, WelcomeContainer, WelcomeImg, WithInputIcon, Wrapper,} 
+    from "./styled.register"
+
+
+
+const Register = (title) => {
+
+    const [values, setValues] = useState({
+        password: "",
+        showPassword: false,
+    });
+
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+      };
+
+    const handlePasswordChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+    return (         
+        <Container>
+            <HeaderLogin /> 
+            <TitleCreateAccount >{title.title}</TitleCreateAccount>
+           <Wrapper>
+               <RightContainer>
+                    <DivLeft>
+                        <Title>Zacro</Title>         
+                    </DivLeft>                
+                   <LoginDescContainer>                      
+                        <Desc> #1 Shopping Platform in the Philippines</Desc>
+                    </LoginDescContainer>                 
+               </RightContainer>
+               <LeftContainer>
+                    <WelcomeContainer>                      
+                            <WelcomeImg   
+                            src="/images/login/welcome.svg" 
+                            alt="" 
+                            />
+                    </WelcomeContainer>
+                   <form onSubmit={handleSubmit}>                
+                       <FormContainer>
+                           <TitleLogin>sign up</TitleLogin>           
+                           <InputContainer>                                
+                                <Input 
+                                placeholder="Email Address" 
+                                type="email"
+                                autoFocus
+                                />
+                           </InputContainer> 
+                           <WithInputIcon>
+                                <Input 
+                                placeholder="Password" 
+                                type={values.showPassword ? "text" : "password"}
+                                onChange={handlePasswordChange}
+                                />
+                                <Visibility onClick={handleClickShowPassword}>             
+                                    {values.showPassword ? 
+                                    <VisibilityOutlined 
+                                    fontSize="small"
+                                    />
+                                     : 
+                                    <VisibilityOffOutlined 
+                                    fontSize="small" 
+                                    /> }
+                                </Visibility>
+                           </WithInputIcon> 
+                           <InputContainer>
+                                 <Input 
+                                 placeholder="Confirm Password" 
+                                 type="password"
+                                 />
+                           </InputContainer>                                                       
+                           <LoginButton>sign up</LoginButton>                                                
+                               <SeparatorContainer>
+                                   <Separator/>
+                                   <Or>Or</Or>
+                                   <Separator/>
+                               </SeparatorContainer>
+                            <SocialContainer>
+                                <SocialButton color="1877f2">
+                                    <Icon><Facebook/></Icon>
+                                    <IconLabel>facebook</IconLabel>
+                                </SocialButton>
+                                <SocialButton color="4285f4" >
+                                    <Icon><Google/></Icon>
+                                    <IconLabel>google</IconLabel>
+                                </SocialButton>
+                                <SocialButton color="000" >
+                                <Icon><Apple/></Icon>
+                                    <IconLabel>apple</IconLabel>
+                                </SocialButton>
+                            </SocialContainer>
+                            <TermsPrivacyContainer>
+                                By signing up, you agree to Zacro's
+                               <TermsPrivacy href="#">
+                                   Terms of Service
+                               </TermsPrivacy>
+                                 &
+                               <TermsPrivacy href="#">
+                                   Privacy
+                               </TermsPrivacy>
+                           </TermsPrivacyContainer>                                                                                                                                                         
+                       </FormContainer>
+                   </form>
+
+                   <SignupContainer>
+                        <LabelNew>
+                            Have an account?
+                                <LabelSignup>
+                                    log in 
+                                </LabelSignup>
+                        </LabelNew>
+                    </SignupContainer>  
+               </LeftContainer>
+           </Wrapper>
+        <Footer/>
+    </Container>      
+    )
+}
+
+export default Register
