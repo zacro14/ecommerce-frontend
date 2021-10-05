f991c2c2bb7cd8fe96e89eaa8637e6240e0cb1a7
Junel <sacrojunel@gmail.com>
2021-10-05 20:42:43 +0800

    add mobile responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,29 @@
-import { FacebookOutlined, GitHub, MailOutline, PhoneAndroidOutlined, RoomOutlined, Twitter } from "@mui/icons-material"
-import {Center, ContactItem, Container,Desc,Left, List, ListItem, Logo, Right, SocialContainer, SocialIcon, Title} from "./styled.footer"
+import { FacebookOutlined, Favorite, GitHub, MailOutline, PhoneAndroidOutlined, RoomOutlined, Twitter } from "@mui/icons-material"
+import {AssetAttrib, AttribContainer, Center, ContactItem, Container,Copyright,CopyrightContainer,Desc,DescContainer,Left, List, ListItem, Logo, Right, SocialContainer, SocialIcon, Title} from "./styled.footer"
 
 
 const Footer = () => {
     return (
        <Container>
+           <DescContainer>
            <Left>
                <Logo>ZACRO</Logo>
                <Desc>
                As Asia’s Online Fashion Destination, 
                we create endless style possibilities 
                through an ever-expanding range of products form the most coveted international and local brands, 
-               putting you at the centre of it all. With ZACRO, You Own Now.
+               putting you at the centre of it all. With <b>ZACRO</b>, You Own Now.
                </Desc>
 
                <SocialContainer>
                    <SocialIcon color ="3b5999">
-                       <FacebookOutlined/>
+                       <FacebookOutlined style={{color: "#fff"}}/>
                    </SocialIcon>
                    <SocialIcon color="24292F">
-                        <GitHub/>
+                        <GitHub style={{color: "#fff"}}/>
                    </SocialIcon>
                    <SocialIcon color="55caee">
-                        <Twitter/>
+                        <Twitter style={{color: "#fff"}}/>
                    </SocialIcon>
                </SocialContainer>
            </Left>
@@ -65,6 +66,25 @@ const Footer = () => {
                              youremail@email.com
                    </ContactItem>                                                  
            </Right>
+           </DescContainer>
+
+           <CopyrightContainer>
+                <Copyright>
+                    &#169; 2021  Made with
+                    <Favorite 
+                    style={{color: "red", fontSize: "20px", margin: "0 5px 0 5px"}}
+                    />
+                    by junel sacro
+                </Copyright>
+                <AttribContainer>
+                    <AssetAttrib>
+                        all images are from unsplash.com
+                    </AssetAttrib>
+                    <AssetAttrib>
+                        all SVG are from  undraw.com
+                    </AssetAttrib>
+                </AttribContainer>            
+           </CopyrightContainer>
        </Container>
     )
 }
--- src/components/footer/styled.footer.js
+++ src/components/footer/styled.footer.js
@@ -1,8 +1,18 @@
 import styled from "styled-components"
+import { mobile } from "../../responsive"
 
-export const Container= styled.footer`
+export const Container= styled.footer`   
+    background-color: #fff;
+    color:#000;
+    margin-top: 1.25rem;
+`;
+
+export const DescContainer = styled.div`
     display: flex;
-    background-color: #000000;
+
+    ${mobile({
+        flexDirection: "column"
+    })}
 `;
 
 
@@ -10,82 +20,121 @@ export const Left= styled.div`
     flex:1;
     display: flex;
     flex-direction: column;
-    color: white;
+    color: inherit;
     padding:1rem; 
 `;
 export const Logo =styled.h1`
     font-weight: 200 ;
-    letter-spacing:5px;
+    letter-spacing:.3125rem;
     cursor: pointer;
-    font-size:50px;
+    font-size:3.125rem;
 `;
 
 export const Desc =styled.p`
-    margin: 20px 0;
+    margin: 1.25rem 0;
     font-weight:200;
-    font-size:13px;
+    font-size:.8125rem;
 `;
 export const SocialContainer = styled.div`
     display: flex;
- 
+    margin-top:1.875rem;
+
+    ${mobile({justifyContent: "center"})}
 `;
 
 export const SocialIcon = styled.div`
     cursor:pointer;
-    width:40px;
-    height:40px;
+    width:2.5rem;
+    height:2.5rem;
     border-radius:50%;
     background-color: #${props=> props.color};
     display: flex;
     align-items: center;
     justify-content:center;
-    color: white;
-    margin-right: 10px;
+    color: inherit;
+    margin-right: .625rem;
     
    
 `;
 
 export const Center= styled.div`
     flex:1;
+    padding: 16px;
 `;
 
 export const Title = styled.h3`
-    color:white;
-    margin: 30px 0;
+    color:inherit;
+    margin: 1.875rem 0;
 `;
     
 export const List = styled.ul`
     margin:0;
     padding: 0;
     list-style:none;
-    color:white;
+    color:inherit;
     display: flex;
-    flex-wrap:wrap; 
-    
+    flex-wrap:wrap;  
   
 `;
 export const ListItem= styled.li`
     width:50%;
     font-weight:200;
-    font-size:13px;
+    font-size:.8125rem;
     cursor: pointer;
-    margin-bottom:5px;
+    margin-bottom:.3125rem;
 
     &:hover{
-        color:#EEC015;
+        text-decoration:underline;
     }
 `;  
 
 export const Right= styled.div`
-flex:1; 
-color:white;
+    flex:1; 
+    color:inherit;
+    padding: 16px;
 `;
 
 export const ContactItem = styled.div`
     display:flex;
     font-weight:200;
-    font-size:13px;
-    margin-bottom:10px;
+    font-size:.8125rem;
+    margin-bottom:.625rem;
     align-items:center;
-    `;
+`;
 
+export const CopyrightContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 2.5rem 0;
+    background-color:#fff;
+    color: #000;
+    font-size: .75rem;
+    text-transform: capitalize;
+    border-top: .0625rem solid #ccc;
+
+    ${mobile({flexDirection: "column", padding: "16px"})}
+`;
+
+export const Copyright = styled.div`
+    flex:1;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+`;
+
+export const AttribContainer = styled.div`
+    display: flex;
+    flex-direction:column;
+    justify-content:flex-end;
+    margin-right:.3125rem;
+
+    ${mobile({
+        alignItems: "center",
+        marginTop: "10px"
+    })}
+`;
+
+export const AssetAttrib = styled.a`
+    text-decoration:none;
+    padding-bottom:.3125rem;
+`;
