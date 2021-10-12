7aa8cd59338b87cfc8211a854af7500165703970
Junel <sacrojunel@gmail.com>
2021-10-12 16:36:01 +0800

    add for mobile /  close

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,15 +1,21 @@
 
-import styled from "styled-components"
+import styled from "styled-components/macro"
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import { mobile } from "../responsive";
+import { useState } from "react";
+
 const Container = styled.div`
     height: 40px;
     background-color:  #EEC015;
     color: #000000;
-    display: flex;
+    display: ${props=> props.close ? "flex" : "none"};
     align-items: center;
     justify-content:space-between;
     font-size:14px;
     font-weight:500;
+    padding: 0 10px;
+    
+    ${mobile({fontSize: "12px"})}
 `
 const AnouncDesc= styled.span`
     flex:1;
@@ -18,23 +24,28 @@ const AnouncDesc= styled.span`
     justify-content: center;
 `;
 const Close =styled.span`
-
     justify-content: flex-end;
+    align-items:center;
     cursor: pointer;
     color: #303F42;
+
+    ${mobile({justifyContent: "center"})}
 `
 
 const Announcement = () => {
-
+    const [close, setClose] = useState(true)
+    const handleClose = () => {
+        setClose(false);
+    }
     return (
-        <Container>
+        <Container close = {close}>
             <AnouncDesc>
                 Super Deal! 
                 Free Shipping 
                 on Orders with Minimum 
                 Spend of  &#8369;199
                 </AnouncDesc>
-                <Close >
+                <Close onClick={handleClose}>
                     <CloseOutlinedIcon />
                 </Close>
         </Container>
