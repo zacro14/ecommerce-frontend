bece33489518ad494dfaeefb30507f4189db9b08
Junel <sacrojunel@gmail.com>
2021-10-31 15:48:30 +0800

    move summary component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,82 +1,57 @@
 import { Suspense, lazy } from "react";
 import { useSelector } from "react-redux"
-import { formatNumber } from "../../helpers/utils"
 import Footer from "../../components/footer/Footer"
 import Navbar from "../../components/navbar/Navbar"
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import EmptyCart from "../../components/cart/emptycart/EmptyCart"
-
+import ItemSummary from "../../components/cart/productsummary/ItemSummary";
 import { 
-    AcceptPaymentContainer,
-    CartContainer, CartTitle, CartWrapper, 
-    CheckoutButton,LeftCartContainer,
-    PaymentImg, PaymentMethodTitle,PaymentsImgContainer,
-    PriceText,RightCartContainer, ShippingfeeText,
-    SubTotalText, SummaryContainer, SummaryInfo, 
-    SummaryInfoTotal, SummaryTitle, 
-    TotalPriceText, TotalText
-    } 
-    from "./styled.cart";
+    CartContainer, 
+    CartTitle, 
+    CartWrapper, 
+    ItemSummaryTitle, 
+    LeftCartContainer,
+    ProductDesc,
+    ProductTitle,
+    RightCartContainer,
+} from "./styled.cart";
+
 
 
 const CartItem = lazy(()=> import("../../components/cart/cartitem/CartItem"));
 
 const Cart = () => {
     const cart = useSelector(state => state.cart)
-    
+
     useDocumentTitle ('Cart-MENS|Corner')
-   
     return (
  <CartContainer>
-     <Navbar />
-     <Suspense fallback = { <div style= {{textAlign : "center", minHeight: "50vh"}}>Loading...</div>}>
-     <CartTitle>shopping bag</CartTitle>
+    <Navbar />
+    <Suspense fallback = { <div style= {{textAlign : "center", minHeight: "50vh"}}>Loading...</div>}>
+    <CartTitle>shopping bag</CartTitle>
       <CartWrapper>       
-        <LeftCartContainer>
+        <LeftCartContainer> 
+            <ItemSummaryTitle>
+                <ProductTitle>Product</ProductTitle>
+                <ProductDesc>unit price</ProductDesc>
+                <ProductDesc>quantity</ProductDesc>
+                <ProductDesc>total price</ProductDesc>
+                <ProductDesc>Action</ProductDesc>
+            </ItemSummaryTitle>
             {
-                cart.cartquantity > 0 ?
+            cart.product.length > 0 ?
             cart.product.map((product)=>(
                  <CartItem products = {product} key={product.id} /> 
             )) : <EmptyCart />
         }        
         </LeftCartContainer>
         <RightCartContainer>
-            <SummaryContainer>
-                <SummaryTitle>Order summary</SummaryTitle>
-                <SummaryInfo>
-                    <SubTotalText>subtotal</SubTotalText>
-                    <PriceText>{formatNumber(cart.total)}</PriceText>
-                </SummaryInfo>
-                <SummaryInfo>
-                    <ShippingfeeText>Estimated shipping fee</ShippingfeeText>
-                    <PriceText>{formatNumber(60)}</PriceText>
-                </SummaryInfo>
-                <SummaryInfoTotal>
-                    <TotalText>total</TotalText>
-                    <TotalPriceText>{formatNumber(cart.total + 60 )}</TotalPriceText>
-                </SummaryInfoTotal>
-                <CheckoutButton>
-                proceed to checkout
-                </CheckoutButton>
-            </SummaryContainer>
-
-            <AcceptPaymentContainer>
-                <PaymentMethodTitle>
-                    accepted payment methods
-                </PaymentMethodTitle>
-                <PaymentsImgContainer>
-                    <PaymentImg 
-                    src="/images/cart/payment-method.png"
-                    alt="payment" 
-                    />
-                </PaymentsImgContainer>
-            </AcceptPaymentContainer>
+            <ItemSummary />
         </RightCartContainer>       
-      </CartWrapper>
-      </Suspense>
-      <Footer/>
+    </CartWrapper>
+    </Suspense>
+    <Footer/>
  </CartContainer>
-    )
-}
+)}
 
 export default Cart
