817b761b9998a14af4f72a8c846a2ec5bc44ee1c
Junel <sacrojunel@gmail.com>
2021-10-31 15:50:28 +0800

    add total

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,3 +1,10 @@
 export const formatNumber = (number) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'PHP' }).format(number);
 }
+export const calculateTotal = (arr) => {
+    if (!arr || arr?.length === 0) return 0;
+  
+    const total = arr.reduce((acc, val) => acc + val, 0);
+  
+    return total.toFixed(2);
+  };
