ae5c3a90c139f509710f912d7f308c2206d89cd6
Junel <sacrojunel@gmail.com>
2021-10-31 15:55:57 +0800

    move

--- src/components/cart/cartitem/cartItem_styled.jsx
+++ src/components/cart/cartitem/cartItem_styled.jsx
@@ -1,48 +1,19 @@
 import styled from 'styled-components/macro'
+import { Remove, Add } from '@mui/icons-material';
 import { mobile } from "../../../responsive"
 import { Amount, FilterColor, SelectSize } from "../../../pages/SingleProduct/styled_viewproduct";
-import { Remove, Add } from '@mui/icons-material';
+import { ItemSummaryTitle} from '../../../pages/Cart/styled.cart';
 
-
-export const ItemSummaryTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  text-transform:capitalize;
-  align-items: center;
-  height: 1.875rem;
-  border-bottom: 1px solid #ccc;
-
-  ${mobile({
-      display: "none",  
-  })};
-`;
-
-export const ProductTitle = styled.div`
-  font-weight:600;
-  font-size:.875rem;
-  width:50%;
-`;
-
-export const ProductDesc= styled(ProductTitle)`
-  text-align:center;
-  width: 12.5%;
-
-  ${mobile({
-      width: "none",  
-  })};
-`;
 
 export const Info = styled(ItemSummaryTitle)`
   display: flex;
   justify-content: space-between;
   min-height: 12.5rem;
-  border-bottom: 1px dashed #ccc;
   ${mobile({
     flexWrap: "wrap",
     minHeight: "225px"
   })};
 `;
-
 
 export const ProductContainer = styled.div`
   display: flex;
@@ -57,6 +28,7 @@ export const ProductContainer = styled.div`
 export const ProductImage = styled.img`
  width: 150px;
  height: 150px;
+ object-fit: cover;
 `;
 
 export const ProductDetails = styled.div`
@@ -66,7 +38,7 @@ export const ProductDetails = styled.div`
 `;
 
 export const Description = styled.span`
-  justify-content: flex-start;
+  justify-content: space-between;
   font-size:.875rem;
   font-weight:${props=>props.fontweight};
   padding: 10px 0;
@@ -74,12 +46,19 @@ export const Description = styled.span`
 
 export const FiltercolorCart = styled(FilterColor)`
   cursor: pointer;
+  width: 25px;
+  height: 25px;
+
+  &:hover{
+    border: 1px solid transparent;
+  }
 `;
 
 export const ColorText = styled.span`
   display: flex;
   flex-direction:row;
-  align-items: center;
+  align-items: baseline;
+  justify-content: space-between;
 `;
 
 export const SizeText = styled(ColorText)`
@@ -133,6 +112,4 @@ export const RemoveItem = styled.div`
       color: #606770;
   }
   ${mobile({padding: "10px",})}
-  `;
-export const AddToWishlist = styled(RemoveItem)`
 `;
