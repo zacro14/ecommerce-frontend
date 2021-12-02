c93a818e7b2a0914059d94589c9ff65114572101
Junel <sacrojunel@gmail.com>
2021-12-02 16:21:02 +0800

    add the fake server

--- src/BASE_URL.js
+++ src/BASE_URL.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const  BASE_URL = "http://192.168.254.155:3001"
+const  BASE_URL = "https://menscorner-fake-server.herokuapp.com/"
 
 export const publicReq = axios.create({
     baseURL: BASE_URL
